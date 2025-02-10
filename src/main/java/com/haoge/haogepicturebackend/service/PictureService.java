970398be8140b9fb541a5e0e8768a42743214864
haoge <1668173696@qq.com>
2025-02-10 21:50:15 +0800

    图片增删改查

--- src/main/java/com/haoge/haogepicturebackend/service/PictureService.java
+++ src/main/java/com/haoge/haogepicturebackend/service/PictureService.java
@@ -1,11 +1,16 @@
 package com.haoge.haogepicturebackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.haoge.haogepicturebackend.model.dto.picture.PictureQueryRequest;
 import com.haoge.haogepicturebackend.model.dto.picture.PictureUpdateRequest;
 import com.haoge.haogepicturebackend.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.haoge.haogepicturebackend.model.entity.User;
 import com.haoge.haogepicturebackend.model.vo.PictureVO;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author haoge
@@ -18,4 +23,36 @@ public interface PictureService extends IService<Picture> {
      */
     PictureVO uploadPicture(MultipartFile multipartFile, PictureUpdateRequest pictureUpdateRequest, User loginUser);
 
+    /**
+     * 获取查询对象
+     *
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    /**
+     * 获取图片包装类（单条）
+     *
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    /**
+     * 获取图片包装类（分页）
+     *
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 校验图片
+     *
+     * @param picture
+     */
+    void validPicture(Picture picture);
 }
--- src/main/java/com/haoge/haogepicturebackend/service/UserService.java
+++ src/main/java/com/haoge/haogepicturebackend/service/UserService.java
@@ -91,4 +91,11 @@ public interface UserService extends IService<User> {
      * @return
      */
     QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    /**
+     * 是否为管理员
+     * @param user
+     * @return
+     */
+    boolean isAdmin(User user);
 }
